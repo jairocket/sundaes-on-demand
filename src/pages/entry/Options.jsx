ea5d1c147e793b445428be75858f33b0e60561bc
Jailson Anjos <jailson.anjos@ifood.com.br>
2022-05-09 21:46:25 -0300

    add alert component and tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,17 +3,23 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        console.log(error);
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
 
   const optionItems = items.map((item) => (
